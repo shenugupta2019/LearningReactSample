2e1905020271bf03530c0b13b70f206f238aec94
Shenu Gupta <Shenu.Gupta@xebia.com>
2022-11-14 14:28:06 +0530

    api fetch implementation react native

---
+++ YourProjectName2/App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import MainApp from './src/Main';
+ export default function App() {
+   return (
+     <MainApp />
+   );
+ }
---
+++ YourProjectName2/src/components/users.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import User from './user';
+const Users = props => {
+    const [isLoading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    getUsers = () => {
+        fetch('https://jsonplaceholder.typicode.com/users/')
+          .then((response) => response.json())
+          .then((json) => setUsers(json))
+          .catch((error) => console.error(error))
+          .finally(() => setLoading(false));
+    }
+    useEffect(() => {
+        setLoading(true);
+        getUsers();
+    }, []);
+    return(
+        <View style={{ padding: 20 }}>
+            {isLoading ? <Text>Loading...</Text> :
+            (
+                <FlatList
+                    data={users}
+                    keyExtractor={({ id }) => id}
+                    renderItem={
+                        ({ item }) =>
+                        <TouchableOpacity
+                            onPress={() =>
+                                props.navigation.navigate('User', {
+                                    id: item.id
+                                })
+                            }
+                        >
+                            <View>
+                                <User user={item} />
+                            </View>
+                        </TouchableOpacity>
+                    }
+                />
+            )}
+        </View>
+    );
+};
+export default Users;
